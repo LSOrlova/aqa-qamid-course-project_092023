2c29838ba17db83b009e9d06b4c97255a0622cdb
Neto test <lsorlova82@gmail.com>
2023-04-24 16:08:59 +0300

    first UI test

--- src/test/java/page/PaymentPage.java
+++ src/test/java/page/PaymentPage.java
@@ -1,6 +1,10 @@
 package page;
 
 import com.codeborne.selenide.SelenideElement;
+import data.Card;
+
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -8,12 +12,13 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class PaymentPage {
+
     private SelenideElement heading = $$("h3").find(text("Оплата по карте"));
-    private SelenideElement cardNumber = $(byText("Номер карты")).parent().$("input__control");
-    private SelenideElement month = $(byText("Месяц")).parent().$("input__control");
-    private SelenideElement year = $(byText("Год")).parent().$("input__control");
-    private SelenideElement owner = $(byText("Владелец")).parent().$("input__control");
-    private SelenideElement cvv = $(byText("CVC/CVV")).parent().$("input__control");
+    private SelenideElement cardNumberField = $(byText("Номер карты")).parent().$("input");
+    private SelenideElement monthField = $(byText("Месяц")).parent().$("input");
+    private SelenideElement yearField = $(byText("Год")).parent().$("input");
+    private SelenideElement ownerField = $(byText("Владелец")).parent().$("input");
+    private SelenideElement cvcField = $(byText("CVC/CVV")).parent().$("input");
     private SelenideElement continueButton = $$("button").find(exactText("Продолжить"));
     private SelenideElement notificationOK = $(".notification_status_ok");
     private SelenideElement notificationError = $(".notification_status_error");
@@ -31,11 +36,11 @@ public class PaymentPage {
     }
 
     public void notificationOkIsVisible() {
-        notificationOK.waitUntil(visible, 15_000);
+        notificationOK.shouldBe(visible, Duration.ofSeconds(15));
     }
 
     public void notificationErrorIsVisible() {
-        notificationError.waitUntil(visible, 15_000);
+        notificationError.shouldBe(visible, Duration.ofSeconds(15));
     }
 
     public boolean inputInvalidIsVisible() {
@@ -43,4 +48,4 @@ public class PaymentPage {
     }
     }
 
-}
+
--- src/test/java/test/ShopTest.java
+++ src/test/java/test/ShopTest.java
@@ -1,8 +1,47 @@
-package ru.netology;
+package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import data.Card;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import page.HomePage;
+import page.PaymentPage;
 
-class ShopTest {
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShopTest {
+    @BeforeEach
+    public void openPage() {
+        String url = System.getProperty("sut.url");
+        open(url);
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/valuesPayment.cvs", numLinesToSkip = 1)
+    @DisplayName("Should show warning pop up during fill in form")
+    void shouldShowWarningIfValueIsIncorrectForPayment(String number, String month, String year, String owner, String cvc, String message) {
+        Card incorrectValuesCard = new Card(number, month, year, owner, cvc);
+        HomePage startPage = new HomePage();
+        PaymentPage paymentPage = startPage.goToPaymentPage();
+        paymentPage.inputData(incorrectValuesCard);
+        assertTrue(paymentPage.inputInvalidIsVisible(), message);
+    }
 
 
 }
