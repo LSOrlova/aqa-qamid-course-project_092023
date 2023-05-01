a5032d236ad135297bf0760da6dd256d0370625f
Neto test <lsorlova82@gmail.com>
2023-05-01 14:10:12 +0300

    new tests were added

--- src/test/java/test/ShopTest.java
+++ src/test/java/test/ShopTest.java
@@ -2,13 +2,12 @@ package test;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
 import data.Card;
+import data.DataGenerator;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import page.CreditPage;
 import page.HomePage;
 import page.PaymentPage;
 
@@ -34,7 +33,7 @@ public class ShopTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/values.cvs", numLinesToSkip = 1)
-    @DisplayName("Should show warning pop up during fill in form")
+    @DisplayName("Should show warning pop up during fill in payment form")
     void shouldShowWarningIfValueIsIncorrectForPayment(String number, String month, String year, String owner, String cvc, String message) {
         Card incorrectValuesCard = new Card(number, month, year, owner, cvc);
         HomePage startPage = new HomePage();
@@ -42,6 +41,53 @@ public class ShopTest {
         paymentPage.inputData(incorrectValuesCard);
         assertTrue(paymentPage.inputInvalidIsVisible(), message);
     }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/values.cvs", numLinesToSkip = 1)
+    @DisplayName("Should show warning pop up during fill in credit form")
+    void shouldShowWarningIfValueIsIncorrectForCredit(String number, String month, String year, String owner, String cvc, String message) {
+        Card incorrectValuesCard = new Card(number, month, year, owner, cvc);
+        HomePage startPage = new HomePage();
+        CreditPage creditPage = startPage.goToCreditPage();
+        creditPage.fillData(incorrectValuesCard);
+        assertTrue(creditPage.inputInvalidIsVisible(), message);
+    }
+    @Test
+    @DisplayName("Should show warning if card expired, payment page")
+    void shouldShowWarningIfCardIsExpiredForPayment() {
+        Card expiredCard = DataGenerator.getInvalidExpDateCard(-1);
+        HomePage startPage = new HomePage();
+        PaymentPage paymentPage = startPage.goToPaymentPage();
+        paymentPage.inputData(expiredCard);
+        assertTrue(paymentPage.inputInvalidIsVisible(),"Should show warning if card expired, payment page");
+    }
 
+    @Test
+    @DisplayName("Should show warning if card expired, credit page")
+    void shouldShowWarningIfCardIsExpiredForCredit() {
+        Card expiredCard = DataGenerator.getInvalidExpDateCard(-1);
+        HomePage startPage = new HomePage();
+        CreditPage creditPage = startPage.goToCreditPage();
+        creditPage.fillData(expiredCard);
+        assertTrue(creditPage.inputInvalidIsVisible(),"Should show warning if card expired, credit page");
+    }
 
+    @Test
+    @DisplayName("Should show warning if expiration date more than 5 years, payment page")
+    void shouldShowWarningIfExpirationDateMoreThan5YearsForPayment() {
+        Card invalidExpDateCard = DataGenerator.getInvalidExpDateCard(61);
+        HomePage startPage = new HomePage();
+        PaymentPage paymentPage = startPage.goToPaymentPage();
+        paymentPage.inputData(invalidExpDateCard);
+        assertTrue(paymentPage.inputInvalidIsVisible(),"Should show warning if expiration date more than 5 years, payment page");
+    }
+
+    @Test
+    @DisplayName("Should show warning if expiration date more than 5 years, credit page")
+    void shouldShowWarningIfExpirationDateMoreThan5YearsForCredit() {
+        Card invalidExpDateCard = DataGenerator.getInvalidExpDateCard(61);
+        HomePage startPage = new HomePage();
+        CreditPage creditPage = startPage.goToCreditPage();
+        creditPage.fillData(invalidExpDateCard);
+        assertTrue(creditPage.inputInvalidIsVisible(),"Should show warning if expiration date more than 5 years, credit page");
+    }
 }
---
+++ src/test/java/test/ShopTestApi.java
@@ -0,0 +1,40 @@
+package test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import data.ApiUtils;
+import data.Card;
+import data.DataGenerator;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class ShopTestApi {
+    Card invalidHolderCard = DataGenerator.getInvalidHolderCard();
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @Test
+    @DisplayName("Shouldn't send payment request with incorrect name")
+    void shouldNotSendPaymentRequestWithIncorrectName() {
+        int statusCode = ApiUtils.getRequestStatusCode(invalidHolderCard, "/api/v1/pay");
+        assertNotEquals(200, statusCode);
+    }
+
+    @Test
+    @DisplayName("Shouldn't send credit request with incorrect name")
+    void shouldNotSendCreditRequestWithIncorrectName() {
+        int statusCode = ApiUtils.getRequestStatusCode(invalidHolderCard, "/api/v1/credit");
+        assertNotEquals(200, statusCode);
+    }
+}
