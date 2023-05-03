949c28976aa245df37274b703fb5090a869e064c
Neto test <lsorlova82@gmail.com>
2023-05-03 14:13:38 +0300

    db tests were added to api chapter

--- src/test/java/test/ShopAPITest.java
+++ src/test/java/test/ShopAPITest.java
@@ -4,11 +4,14 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import data.ApiUtils;
 import data.Card;
 import data.DataGenerator;
+import data.DbUtils;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -28,29 +31,49 @@ public class ShopAPITest {
 
     @Test
     @DisplayName("Should send payment request with approved card")
-    void shouldSendPaymentRequestWithApprovedCard() {
+    void shouldSendPaymentRequestWithApprovedCard() throws SQLException {
         int statusCode = ApiUtils.getRequestStatusCode(DataGenerator.getApprovedCard(), "/api/v1/pay");
         assertEquals(200, statusCode);
+        assertEquals("APPROVED", DbUtils.findPaymentStatus());
     }
 
     @Test
     @DisplayName("Shouldn't send payment request with declined card")
-    void shouldNotSendPaymentWithDeclinedCard() {
+    void shouldNotSendPaymentWithDeclinedCard() throws SQLException {
         int statusCode = ApiUtils.getRequestStatusCode(DataGenerator.getDeclinedCard(), "/api/v1/pay");
         assertEquals(500, statusCode);
+        assertEquals("DECLINED", DbUtils.findPaymentStatus());
     }
 
     @Test
-    @DisplayName("Shouldn't send payment request with incorrect name")
-    void shouldNotSendPaymentRequestWithIncorrectName() {
-        int statusCode = ApiUtils.getRequestStatusCode(invalidHolderCard, "/api/v1/pay");
+    @DisplayName("Shouldn't send payment request with random card")
+    void shouldNotSendPaymentRequestWithIncorrectName() throws SQLException {
+        int statusCode = ApiUtils.getRequestStatusCode(DataGenerator.getFakeCard(), "/api/v1/pay");
         assertNotEquals(200, statusCode);
+        assertEquals("0", DbUtils.countRecords());
     }
 
     @Test
-    @DisplayName("Shouldn't send credit request with incorrect name")
-    void shouldNotSendCreditRequestWithIncorrectName() {
-        int statusCode = ApiUtils.getRequestStatusCode(invalidHolderCard, "/api/v1/credit");
+    @DisplayName("Should send credit request with approved card")
+    void shouldSendCreditRequestWithApprovedCard() throws SQLException {
+        int statusCode = ApiUtils.getRequestStatusCode(DataGenerator.getApprovedCard(), "/api/v1/credit");
+        assertEquals(200, statusCode);
+        assertEquals("APPROVED", DbUtils.findCreditStatus());
+    }
+
+    @Test
+    @DisplayName("Shouldn't send credit request with declined card")
+    void shouldNotSendCreditWithDeclinedCard() throws SQLException {
+        int statusCode = ApiUtils.getRequestStatusCode(DataGenerator.getDeclinedCard(), "/api/v1/credit");
+        assertEquals(500, statusCode);
+        assertEquals("DECLINED", DbUtils.findCreditStatus());
+    }
+
+    @Test
+    @DisplayName("Shouldn't send credit request with random card")
+    void shouldNotSendCreditRequestWithIncorrectName() throws SQLException {
+        int statusCode = ApiUtils.getRequestStatusCode(DataGenerator.getFakeCard(), "/api/v1/credit");
         assertNotEquals(200, statusCode);
+        assertEquals("0", DbUtils.countRecords());
     }
 }
--- src/test/java/test/ShopTest.java
+++ src/test/java/test/ShopTest.java
@@ -3,6 +3,7 @@ package test;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import data.Card;
 import data.DataGenerator;
+import data.DbUtils;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,12 +12,16 @@ import page.CreditPage;
 import page.HomePage;
 import page.PaymentPage;
 
+import java.sql.SQLException;
+
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ShopTest {
     @BeforeEach
-    public void openPage() {
+    public void openPage() throws SQLException {
+        DbUtils.clearTables();
         String url = System.getProperty("sut.url");
         open(url);
     }
@@ -31,34 +36,38 @@ public class ShopTest {
         SelenideLogger.removeListener("allure");
     }
 
+    // PAYMENT PAGE
     @Test
     @DisplayName("Happy path. Should send request by approved card, payment page")
-    void shouldSendRequestSuccessForPayment() {
-        Card expiredCard = DataGenerator.getApprovedCard();
+    void shouldSendRequestSuccessForPayment() throws SQLException {
+        Card approvedCard = DataGenerator.getApprovedCard();
         HomePage startPage = new HomePage();
         PaymentPage paymentPage = startPage.goToPaymentPage();
-        paymentPage.inputData(expiredCard);
+        paymentPage.inputData(approvedCard);
         paymentPage.notificationOkIsVisible();
+        assertEquals("APPROVED", DbUtils.findPaymentStatus());
     }
 
     @Test
     @DisplayName("Should not send request by declined card, payment page")
-    void shouldNotSendRequestDeclinedCardForPayment() {
-        Card expiredCard = DataGenerator.getDeclinedCard();
+    void shouldNotSendRequestDeclinedCardForPayment() throws SQLException {
+        Card declinedCard = DataGenerator.getDeclinedCard();
         HomePage startPage = new HomePage();
         PaymentPage paymentPage = startPage.goToPaymentPage();
-        paymentPage.inputData(expiredCard);
+        paymentPage.inputData(declinedCard);
         paymentPage.notificationErrorIsVisible();
+        assertEquals("DECLINED", DbUtils.findPaymentStatus());
     }
 
     @Test
     @DisplayName("Should not send request by random card, payment page")
-    void shouldNotSendRequestWithWrongCardNumberForPayment() {
+    void shouldNotSendRequestWithWrongCardNumberForPayment() throws SQLException {
         Card wrongRandomCardNumber = DataGenerator.getWrongRandomCardNumber();
         HomePage startPage = new HomePage();
         PaymentPage paymentPage = startPage.goToPaymentPage();
         paymentPage.inputData(wrongRandomCardNumber);
         paymentPage.inputInvalidIsVisible();
+        assertEquals("0", DbUtils.countRecords());
     }
 
     @Test
@@ -131,33 +140,107 @@ public class ShopTest {
         paymentPage.inputInvalidIsVisible();
     }
 
-//    @Test
-//    @DisplayName("Happy path, credit page")
-//    void shouldSendRequestSuccessForCredit() {
-//        Card expiredCard = DataGenerator.getApprovedCard();
-//        HomePage startPage = new HomePage();
-//        CreditPage creditPage = startPage.goToCreditPage();
-//        creditPage.fillData(expiredCard);
-//        creditPage.notificationOkIsVisible();
-//    }
-//
-//    @Test
-//    @DisplayName("Should show warning if card expired, credit page")
-//    void shouldShowWarningIfCardIsExpiredForCredit() {
-//        Card expiredCard = DataGenerator.getInvalidExpDateCard();
-//        HomePage startPage = new HomePage();
-//        CreditPage creditPage = startPage.goToCreditPage();
-//        creditPage.fillData(expiredCard);
-//        assertTrue(creditPage.inputInvalidIsVisible("Истёк срок действия карты"), "Should show warning if card expired, credit page");
-//    }
-//
-//    @Test
-//    @DisplayName("Should show warning if expiration date more than 5 years, credit page")
-//    void shouldShowWarningIfExpirationDateMoreThan5YearsForCredit() {
-//        Card invalidExpDateCard = DataGenerator.getInvalidExpDateCard();
-//        HomePage startPage = new HomePage();
-//        CreditPage creditPage = startPage.goToCreditPage();
-//        creditPage.fillData(invalidExpDateCard);
-//        assertTrue(creditPage.inputInvalidIsVisible("Неверно указан срок действия карты"), "Should show warning if expiration date more than 5 years, credit page");
-//    }
+    // CREDIT PAGE
+    @Test
+    @DisplayName("Happy path. Should send request by approved card, credit page")
+    void shouldSendRequestSuccessForCredit() throws SQLException {
+        Card approvedCard = DataGenerator.getApprovedCard();
+        HomePage homePage = new HomePage();
+        CreditPage creditPage = homePage.goToCreditPage();
+        creditPage.fillData(approvedCard);
+        creditPage.notificationOkIsVisible();
+        assertEquals("APPROVED", DbUtils.findCreditStatus());
+    }
+
+    @Test
+    @DisplayName("Should not send request by declined card, credit page")
+    void shouldNotSendRequestDeclinedCardForCredit() throws SQLException {
+        Card declinedCard = DataGenerator.getDeclinedCard();
+        HomePage homePage = new HomePage();
+        CreditPage creditPage = homePage.goToCreditPage();
+        creditPage.fillData(declinedCard);
+        creditPage.notificationErrorIsVisible();
+        assertEquals("DECLINED", DbUtils.findPaymentStatus());
+    }
+
+    @Test
+    @DisplayName("Should not send request by random card, credit page")
+    void shouldNotSendRequestWithWrongCardNumberForCredit() throws SQLException {
+        Card wrongRandomCardNumber = DataGenerator.getWrongRandomCardNumber();
+        HomePage homePage = new HomePage();
+        CreditPage creditPage = homePage.goToCreditPage();
+        creditPage.fillData(wrongRandomCardNumber);
+        creditPage.inputInvalidIsVisible();
+        assertEquals("0", DbUtils.countRecords());
+    }
+
+    @Test
+    @DisplayName("Should show warning if card expired, credit page")
+    void shouldShowWarningIfCardIsExpiredForCredit() {
+        Card invalidExpDateCard = DataGenerator.getInvalidExpDateCard();
+        HomePage homePage = new HomePage();
+        CreditPage creditPage = homePage.goToCreditPage();
+        creditPage.fillData(invalidExpDateCard);
+        creditPage.inputInvalidIsVisibleExpiredDate();
+    }
+
+    @Test
+    @DisplayName("Should show warning if expiration date more than 5 years, credit page")
+    void shouldShowWarningIfExpirationDateMoreThan5YearsForCredit() {
+        Card invalidExpDateCard = DataGenerator.getInvalidExpDateCardMoreThan5Years();
+        HomePage homePage = new HomePage();
+        CreditPage creditPage = homePage.goToCreditPage();
+        creditPage.fillData(invalidExpDateCard);
+        creditPage.inputInvalidIsVisibleMore5Years();
+    }
+
+    @Test
+    @DisplayName("Should show warning if month 00, credit page")
+    void shouldShowWarningIfMonth00ForCredit() {
+        Card invalidMonthCard = DataGenerator.getInvalidMonth00();
+        HomePage homePage = new HomePage();
+        CreditPage creditPage = homePage.goToCreditPage();
+        creditPage.fillData(invalidMonthCard);
+        creditPage.inputInvalidIsVisible();
+    }
+
+    @Test
+    @DisplayName("Should show warning if month expired, credit page")
+    void shouldShowWarningIfMonthExpiredForCreditt() {
+        Card invalidExpiredMonthCard = DataGenerator.getExpiredMonth();
+        HomePage homePage = new HomePage();
+        CreditPage creditPage = homePage.goToCreditPage();
+        creditPage.fillData(invalidExpiredMonthCard);
+        creditPage.inputInvalidIsVisibleExpiredDate();
+    }
+
+    @Test
+    @DisplayName("Should show warning if year 1 number, credit page")
+    void shouldShowWarningIfYear1numberForCredit() {
+        Card invalidYear1number = DataGenerator.getInvalidYear1number();
+        HomePage homePage = new HomePage();
+        CreditPage creditPage = homePage.goToCreditPage();
+        creditPage.fillData(invalidYear1number);
+        creditPage.inputInvalidIsVisible();
+    }
+
+    @Test
+    @DisplayName("Should show warning if CVV 1 number, credit page")
+    void shouldShowWarningIfCVV1numberForCredit() {
+        Card invalidCVV1number = DataGenerator.getInvalidCVV1number();
+        HomePage homePage = new HomePage();
+        CreditPage creditPage = homePage.goToCreditPage();
+        creditPage.fillData(invalidCVV1number);
+        creditPage.inputInvalidIsVisible();
+    }
+
+    @Test
+    @DisplayName("Should show warning if CVV 2 number, credit page")
+    void shouldShowWarningIfCVV2numberForCredit() {
+        Card invalidCVV2numbers = DataGenerator.getInvalidCVV2numbers();
+        HomePage homePage = new HomePage();
+        CreditPage creditPage = homePage.goToCreditPage();
+        creditPage.fillData(invalidCVV2numbers);
+        creditPage.inputInvalidIsVisible();
+    }
 }
